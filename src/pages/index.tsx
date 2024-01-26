1ebef98b1d685ffcc202e348df977d58c78dba13
Oles Odynets <olchyk98@gmail.com>
2024-01-26 01:09:14 +0100

    chore: setup frontend

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
-import { Heading } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Box } from '@chakra-ui/react'
+import { motion } from 'framer-motion'
+//import { performValidation } from '@/validation-worker'
+
+const MotionBox = motion(Box)
 
 export default function Home () {
   return (
-    <Heading>Hello, World!</Heading>
+    <MotionBox
+      height="40"
+      borderRadius="xl"
+      width="40"
+      bg="red.400"
+      whileHover={{ x: 40 }}
+    />
   )
 }
